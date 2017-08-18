948af2d799012e6fed809d56cfb02dcc3f001477
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2017-08-18 20:48:36 +0200

    Project - Connect to Firebase Database - Display elements in cards list - Add new notes with field verification

---
+++ app/home/home.controller.js
@@ -0,0 +1,37 @@
+angular.module('Colist').controller('homeController', homeController)
+
+homeController.$inject = ['$scope', '$firebaseArray', '$firebaseObject']
+
+function homeController($scope, $firebaseArray, $firebaseObject) {
+
+    /* Variables */
+    let me = this
+    me.notes
+
+    /* Functions */
+    me.init = init
+    me.add = add
+
+    function init() {
+        let elementsRef = firebase.database().ref().child("notes")
+        me.notes = $firebaseArray(elementsRef)
+        me.newNote = {}
+    }
+
+    function add() {
+        if (typeof me.newNote.title === 'undefined') {
+            ons.notification.alert('Title can\'t be empty!')
+        } else {
+            var description = typeof me.newNote.description === 'undefined' ? '' : me.newNote.description
+            me.notes.$add({
+                title: me.newNote.title,
+                description: description,
+                date: new Date(Date.now()).toLocaleDateString()
+            })
+            ons.notification.alert('Added!')
+        }
+
+    }
+
+    init()
+}
---
+++ conf/firebase.sample.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "APIKEY",
+    authDomain: "DOMAIN",
+    databaseURL: "DB_URL",
+    projectId: "ID",
+    storageBucket: "",
+    messagingSenderId: "MSID"
+};
+firebase.initializeApp(config);
