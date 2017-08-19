337a0d5a79faecc9db513e3f88e25ab0e444b151
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2017-08-19 16:22:12 +0200

    Improvements - Better Remove button placement - Remove confirm dialog - Empty title checker

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -6,20 +6,21 @@ function homeController($scope, $firebaseArray, $firebaseObject) {
 
     /* Variables */
     let me = this
+    let elementsRef = firebase.database().ref().child("notes")
     me.notes
 
     /* Functions */
     me.init = init
     me.add = add
+    me.remove = remove
 
     function init() {
-        let elementsRef = firebase.database().ref().child("notes")
         me.notes = $firebaseArray(elementsRef)
         me.newNote = {}
     }
 
     function add() {
-        if (typeof me.newNote.title === 'undefined') {
+        if (typeof me.newNote.title === 'undefined' || me.newNote.title === '') {
             ons.notification.alert('Title can\'t be empty!')
         } else {
             var description = typeof me.newNote.description === 'undefined' ? '' : me.newNote.description
@@ -34,7 +35,14 @@ function homeController($scope, $firebaseArray, $firebaseObject) {
                 description: ''
             }
         }
+    }
 
+    function remove(note) {
+        ons.notification.confirm({ message: 'Are you sure?' }).then(function(response) {
+            if (response) {
+                me.notes.$remove(note)
+            }
+        })
     }
 
     init()
